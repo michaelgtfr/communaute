d721c249a3251bb606264154b403d7c9b515a28d
mickdu62 <mickdu62200@gmail.com>
2021-01-06 18:09:05 +0100

    created the Discution entity unrelated in the Entity folder

---
+++ src/Entity/Discussion.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DiscussionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=DiscussionRepository::class)
+ */
+class Discussion
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateCreate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateCreate(): ?\DateTimeInterface
+    {
+        return $this->dateCreate;
+    }
+
+    public function setDateCreate(\DateTimeInterface $dateCreate): self
+    {
+        $this->dateCreate = $dateCreate;
+
+        return $this;
+    }
+}
