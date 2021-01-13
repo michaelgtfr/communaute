e676c1bdf083e8c43643a48d4ebafcc17fa7614b
mickdu62 <mickdu62200@gmail.com>
2021-01-13 09:41:17 +0100

    setting up of the bootstrap, jquery via composer

--- webpack.config.js
+++ webpack.config.js
@@ -17,10 +17,10 @@ Encore
     /*
      * ENTRY CONFIG
      *
-     * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+     * Each entry will result in one JavaScript file (e.g. base.js)
+     * and one CSS file (e.g. base.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    .addEntry('base', './assets/base.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
