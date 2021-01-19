4ef6e496100f3555e889e1ca73c78ef161f8b386
mickdu62 <mickdu62200@gmail.com>
2021-01-19 08:39:06 +0100

    creation in the RegisterController, allow to display the register page

---
+++ src/Form/RegisterForm.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mickd
+ * Date: 18/01/2021
+ * Time: 18:14
+ */
+
+namespace App\Form;
+
+
+class RegisterForm
+{
+
+}
---
+++ src/Treatment/RegisterTreatment.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mickd
+ * Date: 18/01/2021
+ * Time: 20:34
+ */
+
+namespace App\Treatment;
+
+
+class RegisterTreatment
+{
+
+}
