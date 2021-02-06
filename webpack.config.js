7a2774b7afeda2e18cb028908ab1ebe58ef0d081
mickdu62 <mickdu62200@gmail.com>
2021-02-06 15:38:20 +0400

    update the call bootstrap because bug in the application while the developpement

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ Encore
      * ENTRY CONFIG
      *
      * Each entry will result in one JavaScript file (e.g. base.js)
-     * and one CSS file (e.g. base.css) if your JavaScript imports CSS.
+     * and one CSS file (e.g. base.scss) if your JavaScript imports CSS.
      */
     .addEntry('base', './assets/base.js')
     .addEntry('login', './assets/login.js')
@@ -59,7 +59,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
