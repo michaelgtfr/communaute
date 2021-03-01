bb11d8365ed57c873e5841ac90e735e9ad8a989b
mickdu62 <mickdu62200@gmail.com>
2021-03-01 13:27:02 +0400

    update for bug with travis

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,5 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
+    FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['dev'=> true,'test' => true],
 ];
